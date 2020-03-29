bbed738f59a4ec2e61e213adbf0e50bc532ae5f6
Richard Antao <richardmantao@gmail.com>
2020-03-29 12:23:09 -0400

    Converted to functional component

--- views/src/components/pages/Login/Login.js
+++ views/src/components/pages/Login/Login.js
@@ -1,88 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import { Container } from "reactstrap";
 
-import { connect } from "react-redux";
-import { login } from "../../../actions/auth/auth";
-import { clearErrors } from "../../../actions/auth/errors";
-import PropTypes from "prop-types";
-
 import Nav from '../../../components/organisms/Nav/';
-
 import Signin from "../../reactors/Signin";
-
-
 
 import "./Login.scss";
 
-class Login extends Component {
-    state = {
-        email: "",
-        password: ""
-    };  
-
-    static propTypes = {
-        error: PropTypes.object.isRequired,
-        login: PropTypes.func.isRequired,
-        clearErrors: PropTypes.func.isRequired
-    };
-
-    componentDidMount() {
-        const { clearErrors } = this.props;
-        clearErrors();
-    };
-
-    componentDidUpdate(prevProps) {
-        const { error } = this.props;
-
-        if(error !== prevProps.error) {
-            if(error.id === "LOGIN_FAILED") {
-                this.setState({ message: error.message.message });
-            } else {
-                this.setState({ message: null });
-            };
-        };
-    };
-
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-
-        const { login } = this.props;
-        const { email, password } = this.state;
-
-        login({ email, password });
-    };
-
-    render() {
-        const { email, password } = this.state;  
-
-        return (
-            <div>
-                <Helmet>
-                    <meta name="description" content=""/>
-                    <meta name="keywords" content=""/>
-                    <title>Login</title>
-                </Helmet>
-                <Nav/>
-                <Container>
-                    <h1>Login</h1>
-                    <Signin/>
-                    <a href="/login">Forgot Password</a>
-                </Container>
-
-            </div>
-        );
-    };
+export default () => {
+    return (
+        <>
+            <Helmet>
+                <meta name="description" content=""/>
+                <meta name="keywords" content=""/>
+                <title>Login</title>
+            </Helmet>
+            <Nav/>
+            <Container>
+                <h1>Login</h1>
+                <Signin/>
+                <a href="#forgot-password">Forgot Password</a>
+            </Container>
+        </>  
+    );
 };
-
-const mapStateToProps = state => ({
-    error: state.error
-});
-
-const mapDispatchToProps = { login, clearErrors };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- views/src/components/pages/Register/Register.js
+++ views/src/components/pages/Register/Register.js
@@ -1,87 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import Helmet from "react-helmet";
 import { Container } from "reactstrap";
 
-import { connect } from "react-redux";
-import { register } from "../../../actions/auth/auth";
-import { clearErrors } from "../../../actions/auth/errors";
-import PropTypes from "prop-types";
-
 import Nav from '../../../components/organisms/Nav';
-
 import Signup from "../../reactors/Signup";
 
 import "./Register.scss";
 
-class Register extends Component {
-    state = {
-        first: "",
-        last: "",
-        email: "",
-        password: ""
-    };
-
-    static propTypes = {
-        error: PropTypes.object.isRequired,
-        register: PropTypes.func.isRequired,
-        clearErrors: PropTypes.func.isRequired
-    };
-
-    async componentDidMount() {
-        const { clearErrors } = this.props;
-        await clearErrors();
-    };
-
-    componentDidUpdate(prevProps) {
-        const { error } = this.props; 
-
-        if(error !== prevProps.error) {
-            if(error.id === "REGISTER_FAILED") {
-                this.setState({ message: error.message.message });
-            } else {
-                this.setState({ message: null });
-            };
-        };
-    };
-
-    handleChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-
-    handleSubmit = e => {
-        e.preventDefault();
-    
-        const { register } = this.props;
-        const { first, last, email, password } = this.state;
-
-        register({ first, last, email, password });
-    };  
-
-    render() {
-        const { first, last, email, password, message } = this.state;
-
-        return (
-            <>
-                <Helmet>
-                    <meta name="description" content="New users can register an account" />
-                    <meta name="keywords" content="" />
-                    <title>Register</title>
-                </Helmet>
-                <Nav/>
-                <Container>
-                    <h1>Register For An Account</h1>
-                    <Signup />
-                </Container>
-            </>
-        );
-    };
+export default () => {
+    return (
+        <>
+            <Helmet>
+                <meta name="description" content="New users can register an account" />
+                <meta name="keywords" content="new, users, register, account, profile" />
+                <title>Register</title>
+            </Helmet>
+            <Nav/>
+            <Container>
+                <h1>Register</h1>
+                <Signup />
+            </Container>
+        </>
+    );
 };
-
-const mapStateToProps = state => ({
-    error: state.error
-});
-
-const mapDispatchToProps = { register, clearErrors };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Register);
-

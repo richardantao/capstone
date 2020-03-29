aac4609ab10df48e8d2eb61d7ad46d62ac69ddc3
Richard Antao <richardmantao@gmail.com>
2020-03-29 11:08:08 -0400

    Updated options

--- config/db.js
+++ config/db.js
@@ -8,16 +8,11 @@ const options = {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    
-    //useFindAndModify: false,
-    //autoIndex: false, // Don't build indexes
-    //reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    //poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    //bufferMaxEntries: 0,
+    autoIndex: false,
+    bufferMaxEntries: 0,
     connectTimeoutMS: 10000, 
-    socketTimeoutMS: 45000
-    // family: 4 // Use IPv4, skip trying IPv6 
+    socketTimeoutMS: 45000,
+    family: 4 // Use IPv4, skip trying IPv6 
 }
 
 mongoose.connect(mongoDB, options)
